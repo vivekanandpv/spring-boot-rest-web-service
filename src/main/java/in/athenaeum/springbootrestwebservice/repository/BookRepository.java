e2bb86eee5a2ff839b0240ecae844ca162eeaf0d
vivek <vivek@athenaeum.in>
2021-05-26 20:43:29 +0530

    done

--- src/main/java/in/athenaeum/springbootrestwebservice/repository/BookRepository.java
+++ src/main/java/in/athenaeum/springbootrestwebservice/repository/BookRepository.java
@@ -4,6 +4,21 @@ import in.athenaeum.springbootrestwebservice.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    //  Derived queries are the most appreciated features of Spring Data JPA
+    //  Derived query methods start with: find, query, read, count, get
+    //  IntelliJ offers an excellent support for derived queries.
+
+    //  Comparison: Like, Containing, Between, GreaterThan, etc.
+
+    //  OrderBy<Attr>Asc/OrderBy<Attr>Desc
+
+    //  To limit: First<N>By, Top<N>By
+
+    List<Book> findBookByTitle(String title);
+    Optional<Book> findFirstByTitleAndNPages(String title, int nPages);
 }
