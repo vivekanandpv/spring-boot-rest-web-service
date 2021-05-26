6a9838ade6d59bdb48980d2af3578fd39c8c4b67
vivek <vivek@athenaeum.in>
2021-05-26 21:04:59 +0530

    done

--- src/main/java/in/athenaeum/springbootrestwebservice/repository/BookRepository.java
+++ src/main/java/in/athenaeum/springbootrestwebservice/repository/BookRepository.java
@@ -2,6 +2,8 @@ package in.athenaeum.springbootrestwebservice.repository;
 
 import in.athenaeum.springbootrestwebservice.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,16 +11,36 @@ import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    //  Derived queries are the most appreciated features of Spring Data JPA
-    //  Derived query methods start with: find, query, read, count, get
-    //  IntelliJ offers an excellent support for derived queries.
+    //  When @Query is used, the more efficient or flexible query can be constructed
+    //  especially useful in complex situations.
 
-    //  Comparison: Like, Containing, Between, GreaterThan, etc.
+    //  JPQL (Java Persistence Query Language) queries are database agnostic, are
+    //  easy to reason about (domain model as opposed to the table structure), but
+    //  do not support complete SQL. In extreme cases, writing native queries makes
+    //  more sense.
 
-    //  OrderBy<Attr>Asc/OrderBy<Attr>Desc
+    //  More info:
+    //  https://docs.oracle.com/html/E13946_04/ejb3_langref.html
+    //  https://docs.oracle.com/javaee/6/tutorial/doc/bnbtg.html
 
-    //  To limit: First<N>By, Top<N>By
+    @Query("FROM Book where title= ?1")
+        //?1 is the positional bind parameter
+    List<Book> findByTitle(String title);
 
-    List<Book> findBookByTitle(String title);
-    Optional<Book> findFirstByTitleAndNPages(String title, int nPages);
+    @Query("FROM Book where title = ?1 and nPages >= ?2")
+    List<Book> findByTitleAndPages(String title, int minPages);
+
+    //  for sorting ORDER BY <Attr> ASC/DESC
+
+    //  Projection
+    @Query("SELECT b FROM Book b where b.title = ?1 and b.nPages >= ?2")
+    List<Book> findByTitleAndPagesAnother(String title, int minPages);
+
+    @Query("SELECT b FROM Book b where b.title = :title and b.nPages >= :minPages")
+    List<Book> findByTitleAndPagesNamedBindParameters(@Param("title") String title, @Param("minPages") int minPages);
+
+
+    //  Native queries
+    @Query(value = "SELECT * FROM book WHERE title LIKE :title", nativeQuery = true)
+    List<Book> findByTitleNative(@Param("title") String title);
 }
