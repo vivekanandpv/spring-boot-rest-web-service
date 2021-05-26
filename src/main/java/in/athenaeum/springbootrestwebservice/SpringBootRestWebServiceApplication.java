9e60067a3d26980682505beab359562a2909990c
vivek <vivek@athenaeum.in>
2021-05-26 10:22:26 +0530

    Initial commit

---
+++ src/main/java/in/athenaeum/springbootrestwebservice/SpringBootRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package in.athenaeum.springbootrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/athenaeum/springbootrestwebservice/SpringBootRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.athenaeum.springbootrestwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
